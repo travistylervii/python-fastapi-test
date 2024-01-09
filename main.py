688f330d37b3c251097379f898362d462d1da8c6
Travis <travis@netronline.com>
2024-01-09 14:44:04 -0700

    add to repo

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/ytid/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "Some Query": q}
+
+    #processes youtube transcript from id
+   
+    #return transcript string
+
+    #testing
+
+    speak = "I have nothing to say"
+
+
+    if item_id < 10:
+        speak = "Item is less than 10"
+    if item_id >= 10 and item_id < 20:
+        speak = "Item is between 10 and 20"
+    if item_id >= 20:
+        speak = "item is 20 or over"
+
+    return {"item_id": item_id, "output": speak}
